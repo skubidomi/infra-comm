03a279c5c1b113719bb25662659ba353c41a4892
Domonkos Skublics <skubidomi@gmail.com>
2020-05-29 17:11:15 +0200

    Use and check the new functions

--- main.c
+++ main.c
@@ -14,24 +14,22 @@ int main(void)
 	DDRD |= (1 << DDD6); // data direction register for IR LED
 	DDRB |= (1 << DDB5); // internalLED
 	
-	DDRD &= ~(1 << DDD7); // data direction register for the receiver, input is like a button with pull up resistor
-	
 	PWM_Init();
-	Timer_Init();
+	Timer1_Init();
 	UART_Init();
+	
+
+	Receiver_Init();
+	Timer2_Init();
+	
 	sei();
 	
 	// ToDo1: organize the ddr macros into the init functions, ir led pin, internal led pin, and button pin
 	// ToDo2: organize the init functions into a separate Init function here
-	serialWrite("hello");
+
 	IR_SendCode(0x20df10ef);
     while (1) 
     {
-		if(!(PIND & (1 << PIND7)))
-		{
-			INTERNAL_LED_TOGGLE;
-			serialWrite("now");
-		}
     }
 }
 
